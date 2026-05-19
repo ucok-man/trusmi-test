99557702468b9b307c0e4f2bea19c36049476192
ucok-man <ucokkocu411@gmail.com>
2026-05-19 16:35:56 +0700

    fix(prisma): remove dotenv dependency in prisma config for production environment & update seed command

--- web/prisma.config.ts
+++ web/prisma.config.ts
@@ -1,11 +1,10 @@
-import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: "tsx prisma/seed.ts",
+    seed: "bun prisma/seed.ts",
   },
   datasource: {
     url: env("DATABASE_URL"),
